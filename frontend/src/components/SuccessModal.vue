a788c38315f94515055e1336ce8fdf497951adf7
DeveloperMdIt <developer@md-it-solutions.de>
2025-12-17 12:47:28 +0100

    Add missing headlessui dependency and fix build error

--- frontend/src/components/SuccessModal.vue
+++ frontend/src/components/SuccessModal.vue
@@ -2,7 +2,7 @@
 import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
 import { CheckIcon } from '@heroicons/vue/24/outline'
 
-const props = defineProps<{
+defineProps<{
   show: boolean
   title: string
   message: string
